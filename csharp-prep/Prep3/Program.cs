acb8c6f21682b0b8f41a97ab67dd0acdf6cf34c3
owusub230 <owu23006@byui.edu>
2024-02-11 06:58:45 +0000

    new

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,33 +1,84 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
+        // Create a scripture
+        var scripture = new Scripture("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
 
-        int guess = -1;
+        // Display initial scripture
+        DisplayScripture(scripture);
 
-        // We could also use a do-while loop here...
-        while (guess != magicNumber)
+        // Hide words until all are hidden
+        while (!scripture.AllWordsHidden)
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            Console.WriteLine("Press Enter to continue or type 'quit' to exit:");
+            string input = Console.ReadLine();
 
-            if (magicNumber > guess)
-            {
-                Console.WriteLine("Higher");
-            }
-            else if (magicNumber < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+            if (input.ToLower() == "quit")
+                break;
 
-        }                    
+            HideRandomWords(scripture);
+            DisplayScripture(scripture);
+        }
+
+        Console.WriteLine("All words in the scripture are hidden. Press any key to exit.");
+        Console.ReadKey();
+    }
+
+    static void DisplayScripture(Scripture scripture)
+    {
+        Console.Clear();
+        Console.WriteLine(scripture.Reference);
+        Console.WriteLine(scripture.HiddenText);
+    }
+
+    static void HideRandomWords(Scripture scripture)
+    {
+        scripture.HideRandomWords();
+    }
+}
+
+public class Scripture
+{
+    public string Reference { get; }
+    private List<string> Words { get; }
+    private List<int> HiddenWordIndices { get; }
+    
+    public string HiddenText
+    {
+        get
+        {
+            var hiddenText = new List<string>(Words);
+            foreach (var index in HiddenWordIndices)
+            {
+                hiddenText[index] = new string('_', hiddenText[index].Length);
+            }
+            return string.Join(" ", hiddenText);
+        }
+    }
+
+    public bool AllWordsHidden => HiddenWordIndices.Count == Words.Count;
+
+    public Scripture(string reference, string text)
+    {
+        Reference = reference;
+        Words = text.Split(' ').ToList();
+        HiddenWordIndices = new List<int>();
+    }
+
+    public void HideRandomWords()
+    {
+        Random random = new Random();
+        int randomIndex;
+        do
+        {
+            randomIndex = random.Next(Words.Count);
+        } while (HiddenWordIndices.Contains(randomIndex));
+
+        HiddenWordIndices.Add(randomIndex);
     }
 }
