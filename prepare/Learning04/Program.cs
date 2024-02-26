fa93762c2bddca305d0a0ec338fde223c49695b6
owusub230 <owu23006@byui.edu>
2024-02-26 01:50:14 +0000

    Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,80 @@
 using System;
 
+// Base class for all assignments
+public class Assignment
+{
+    // Member variables
+    protected string _studentName;
+    protected string _topic;
+
+    // Constructor
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    // Method to return a summary of the assignment
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+}
+
+// Derived class for Math assignments
+public class MathAssignment : Assignment
+{
+    // Additional member variable
+    private string _textbookSection;
+    private string _problems;
+
+    // Constructor
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    // Method to display the Math homework list
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
+
+// Derived class for Writing assignments
+public class WritingAssignment : Assignment
+{
+    // Additional member variable
+    private string _title;
+
+    // Constructor
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    // Method to get the writing information
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {_studentName}";
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Testing MathAssignment class
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        // Testing WritingAssignment class
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
